c073f9de45af8b35eeca42e126d54010ae70d370
jacsonrai <jacksonrai1996@gmail.com>
2024-07-16 23:37:55 +0545

    upload api created

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,20 @@ import cors from "cors";
 import router from "./routers";
 import session from "express-session";
 import { authProvider } from "./authprovider/authprovider";
+import multer from "multer";
+import {
+    BlobServiceClient,
+    StorageSharedKeyCredential,
+} from "@azure/storage-blob";
+import { Readable } from "stream";
 
 const app: Express = express();
+const upload = multer({ storage: multer.memoryStorage() });
+
+const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME ?? "";
+const accountKey = process.env.AZURE_STORAGE_ACCOUNT_KEY ?? "";
+const containerName = process.env.AZURE_CONTAINER_NAME ?? "";
+const sasToken = process.env.SAS_TOKEN ?? "";
 
 //middleware
 app.use(
@@ -20,18 +32,56 @@ app.get("/", (req: Request, res: Response) => {
     const data = { message: "Hello from the API!" };
     res.json(data);
 });
-authProvider.syncUsers();
-// app.post("/webhook-endpoint", (req, res) => {
-//     console.log(req);
-//     if (req.query.validationToken) {
-//         // Respond to the validation request
-//         res.status(200).send(req.query.validationToken);
-//     } else {
-//         // Handle regular webhook notifications
-//         console.log("Webhook notification received:", req.body);
-//         res.sendStatus(200);
-//     }
+// const containerClient =
+//     blobServiceClient.getContainerClient(containerName);
+// const blockBlobClient = containerClient.getBlockBlobClient(fileName);
+// await blockBlobClient.uploadData(req.file.buffer, {
+//     blobHTTPHeaders: { blobContentType: fileType },
 // });
+
+authProvider.syncUsers();
+
+const sharedKeyCredential = new StorageSharedKeyCredential(
+    accountName,
+    accountKey
+);
+const blobServiceClient = new BlobServiceClient(
+    `https://${accountName}.blob.core.windows.net`,
+    sharedKeyCredential
+);
+const containerClient = blobServiceClient.getContainerClient(containerName);
+
+async function extractMetaData(reqFile: any) {
+    const fileType = reqFile.mimetype.split("/")[1];
+    const fileName =
+        `${reqFile.originalname}-${Date.now()}` ||
+        `image-${Date.now()}.${fileType}`;
+
+    return { fileName, fileType };
+}
+async function uploadImageStream(blobName: string, dataStream: Readable) {
+    const blobClient = containerClient.getBlockBlobClient(blobName);
+    await blobClient.uploadStream(dataStream);
+    return blobClient.url;
+}
+app.post("/upload", upload.single("file"), async (req: any, res: any) => {
+    let fileName: string;
+    let fileType: string;
+    try {
+        const metaData = await extractMetaData(req.file);
+        fileName = metaData.fileName;
+        fileType = metaData.fileType;
+        const dataStream = new Readable();
+        dataStream.push(req.file.buffer);
+        dataStream.push(null);
+        const imageUrl = await uploadImageStream(fileName, dataStream);
+        console.log(imageUrl, "url");
+
+        res.status(200).send("File uploaded successfully");
+    } catch (error: any) {
+        res.status(500).send(`Error uploading file: ${error.message}`);
+    }
+});
 
 app.use(
     session({
